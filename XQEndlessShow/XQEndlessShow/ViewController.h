af7a4ed1827a71ba2be2db1272a2a834c6c6d371
huangxq <huangxq@wehgc.com>
2017-03-31 12:44:29 +0800

    我的第一个GitHub项目

---
+++ XQEndlessShow/XQEndlessShow/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XQEndlessShow
+//
+//  Created by HCZH on 17/3/31.
+//  Copyright © 2017年 hxq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ XQEndlessShow/XQEndlessShow/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  XQEndlessShow
+//
+//  Created by HCZH on 17/3/31.
+//  Copyright © 2017年 hxq. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    NSLog(@"我的第一个GitHub项目");
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
